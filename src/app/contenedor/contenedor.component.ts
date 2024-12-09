257050f1cfa9ac29182a71573c0f5b0105383228
Alejandro <99300316+Lexvg93@users.noreply.github.com>
2024-12-09 09:35:12 +0000

    contenedor
    se rellena el componente contendor

--- src/app/contenedor/contenedor.component.ts
+++ src/app/contenedor/contenedor.component.ts
@@ -16,7 +16,35 @@ export class ContenedorComponent implements OnInit{
   private _pokemonService= inject(PokemonApiService)
   pokemones:any[]=[];
   ngOnInit(): void {
-    
+    this._contenedorService.pokemonesTipo.subscribe(poke=>{
+      if(poke===''){
+        for(let i=1; i<=151;i++){
+          this._pokemonService.getPokemon(i).subscribe({
+            next:(data)=>{
+              this.pokemones.push(data);
+              this.copiaPoke.push(data);
+            }
+          })
+        }
+        this.copiaPoke.sort((a,b)=>{
+          return a.id - b.id;
+        })
+      }else{
+          this.pokemones.length = 0;
+          if(poke !== 'ver-todos'){
+            for(let i =0; i<this.copiaPoke.length;i++){
+              const tipos = this.copiaPoke[i].types.map((type:any) =>type.type.name);
+              if(tipos.some((tipo:any)=> tipo.includes(poke))){
+                this.pokemones.push(this.copiaPoke[i]);
+              }
+            }
+          }else{
+            for(let i =0; i<this.copiaPoke.length;i++){
+              this.pokemones.push(this.copiaPoke[i]);
+            }
+          }
+      }
+    })
   }
    
 }
